b47e8647f0383f134fb4a74bc69dc63f169b7b68
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2023-09-05 18:05:32 +0300

    Fix error in task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,7 @@
 
 const decrementBtn = document.querySelector(`[data-action="decrement"]`);
 const incrementBtn = document.querySelector(`[data-action="increment"]`);
+const counter = document.querySelector("#value");
 
 let counterValue = 0;
 
